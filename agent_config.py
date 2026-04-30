16b5eefb72d93e7f67ab8930ca32d00e78d2ff08
Dawood Ayaz <dawoodayaz18@gmail.com>
2026-04-30 16:43:53 +0500

    Configure external client with openai sdk

---
+++ agent_config.py
@@ -0,0 +1,37 @@
+from agents import AsyncOpenAI,OpenAIChatCompletionsModel, RunConfig, set_tracing_disabled
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+API_KEY = os.getenv("GEMINI_SEC_KEY")
+
+set_tracing_disabled(disabled=True)
+
+external_client = AsyncOpenAI(
+    api_key=API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+    # base_url="https://api.navy"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.5-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True,
+)
+
+
+
+# external_client = AsyncOpenAI(
+#     api_key=API_KEY,
+#     base_url="https://api.groq.com/openai/v1/",
+# )
+
+# model = OpenAIChatCompletionsModel(
+#     model="qwen/qwen3-32b",
+#     openai_client=external_client
+# )
